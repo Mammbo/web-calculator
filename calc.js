83a96f732ccf7a3c353ca9c2542386ded3b21eb8
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-07-06 10:49:09 -0500

    set up structure for js

--- calc.js
+++ calc.js
@@ -1,45 +1,12 @@
-// functiionality  
-
 // gloabl vars for displaying
-let displayNum1 = 0
-let displayNum2 = 0
-let displayOperator = '' 
-
-function addition(num1, num2) {
-    return num1 + num2 
-}
-
-function subtraction(num1, num2) {
-    return num1 - num2
-}
-
-function multiplication(num1, num2) {
-    return num1 * num2
-}
-
-function division(num1, num2) {
-    return num1 / num2 
-}
-
-//operation function 
-
-function operation(displayNum1, displayOperator, displayNum2) {
-    if (displayOperator === '+'){
-        answer = addition(displayNum1, displayNum2) 
-    } else if (displayOperator === '-') {
-        answer = subtraction(displayNum1, displayNum2)
-    } else if (displayOperator === '*') {
-        answer = multiplication(displayNum1, displayNum2)
-    } else if (displayOperator === '/') {
-        answer = division(displayNum1, displayNum2)
-    } 
-    return answer
-}
-
-
-
+let currentInput = '';
+let displayNum1 = null;
+let displayNum2 = null;
+let currentOperator = null; 
 
 //UI Elements 
+//display 
+const display = document.getElementById("display")
 //Numbers
 const num7 = document.getElementById("7")
 const num8 = document.getElementById("8")
@@ -65,5 +32,64 @@ const add = document.getElementById("addition")
 const clear = document.getElementById("clear")
 const posNeg = document.getElementById("negPos")
 const percent = doucment.getElementById("percentage")
+
+//event listners 
+
+
+//functionality 
+function inputNumber(number) {
+    currentInput += number;
+    display.textContent = `${currentInput}`;
+}
+
+function setOperator(operator) {
+
+}
+
+function turnPosNeg() {
+    //parse current input to see if its a flot
+    //if so add neg to the currentInput that matches the operand
+    //if neg is present when clicked, take the neg off 
+}
+
+function percent() {
+    //parse current input to see if its a float 
+    //if so add a decimal by 2 spaces to the currentInput to change the display and the var that matches that input 
+    //if clicked again, add zeros and move two spaces to the right
+}
+
+function decimal() {
+    //parse current input to see if its a float 
+    //if so add a decimal 
+    //if clicked again it will do nothing on the currentInput because decimal is present
+    //if clicked on new num it will add the decimal 
+}
+
+function calculate() {
+
+}
+
+
+function operation(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return num1 + num2 
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            return num1 / num2
+        default:
+            return null;
+    }
+}
+
+
+function clearDisplay() {
+    // set all global vars to default 
+    //display.text content is none 
+
+}
 
 
